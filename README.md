3c48cf8c5ef3813695694195d6f7edf23fb0f576
RonakSurana-2001 <ronaksurana2017@gmail.com>
2024-07-17 14:10:45 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,67 @@
+## Objective
+A basic admin panel that includes user registration, login, logout
+functionalities, and CRUD operations on users.
+## Tech Stack
+
+**Client:** React, TailwindCSS, Redux
+
+**Server:** Node, Express, Zod (Validatio), Mongoose
+
+**Database:** MongoDB
+
+## Features
+
+- User Can Register and Login
+- Admin can Add new user, delete user, view all users and update details of any user
+- Password Protected using Bcryt JS and Authorization using JSON Web Token
+
+## Video Demonstration
+
+
+## Note
+- When the user registers that individual is a normal user
+- Admin has rights Make someone admin or not
+- The all users table is just shown to admin
+- An email id-admin@mail.com and password-admin is set up of testing purposes
+## Installation
+
+Clone the Repository
+
+```bash
+git clone https://github.com/RonakSurana-2001/HealthTech.git
+```
+For Frontend
+```bash
+cd frontend
+npm install
+npm start
+```
+
+For Backend
+```bash
+cd Backend
+npm install
+node index.js
+```
+
+Environment Files in Backend
+```bash
+JWT_SECRET_KEY=<JWT_SECRET_TOKEN>
+MONGO_URL=<MONGO_DB_LINK>
+```
+
+Start the Developement Server
+```bash
+npm run dev
+```
+## Live Site
+
+<a href="">
+Live site</a>  
+
+Backend is deployed on render and database on mongodb atlas free plan so data fetching from backend takes time. So after you perform any action you have to wait.
+
+## Authors
+
+ [@Ronak Surana](https://www.linkedin.com/in/ronak-surana-944550205/)
+
---
+++ frontend/src/components/SignInComponent.js
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import toast from 'react-hot-toast';
+import { link } from '../BackendLink/linkfile';
+
+import { useDispatch } from 'react-redux'
+import { authActions } from "../redux/store";
+
+function SignInComponent() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate()
+
+    const dispatch = useDispatch()
+
+    const [isLoading, setisLoading] = useState(false)
+
+    console.log(isLoading)
+
+    // console.log(userEmail.current.value,userPassword.current.value)
+
+    const handleSubmit = async () => {
+        setisLoading(true)
+        try {
+            const user = await axios.post(`${link}/user/userLogin`, {
+                email,
+                password
+            })
+            if (user.data.success) {
+                localStorage.setItem("email", email)
+                localStorage.setItem("token", user.data.jwtToken)
+                localStorage.setItem("isAdmin", user.data.isAdmin)
+                toast.success("User Logged Successfully")
+                dispatch(authActions.login())
+                setisLoading(false)
+                navigate("/userdetails")
+            }
+            else {
+                setisLoading(false)
+                toast.error(user.data.message)
+            }
+        } catch (err) {
+            setisLoading(false)
+            console.log(err)
+        }
+    }
+
+    return (
+        <>
+            {!isLoading ? <div className='h-screen flex flex-row items-center justify-center'>
+                <div className='grid grid-row-4 justify-center bg-blue-300 rounded-lg max-md:w-4/5 w-2/6 p-3'>
+                    <div className='flex flex-col items-center space-y-6 p-2'>
+                        <div className='max-md:text-2xl text-4xl'>Email</div>
+                        <input type="text" placeholder='Email' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className='flex flex-col items-center space-y-6 p-2'>
+                        <div className='max-md:text-2xl text-4xl'>Password</div>
+                        <input type="text" placeholder='Password' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className='flex flex-col items-center p-2'>
+                        <button className='text-xl bg-green-600 p-3 rounded-lg' onClick={async () => {
+                            handleSubmit()
+                        }}>Submit</button>
+                    </div>
+                    <div className='flex flex-col items-center space-y-2 p-2 text-center'>
+                        <Link to='/signup' className='max-md:text-sm text-3xl'>Not a user register</Link>
+                    </div>
+                </div>
+            </div> : <p className='text-center text-red-600 text-6xl'>Loading...</p>}
+        </>
+    )
+}
+
+export default SignInComponent
